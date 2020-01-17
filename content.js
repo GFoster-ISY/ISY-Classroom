202ba2082393891be3136c7fa8d9d44a0cd7f6cf
GFoster-ISY <48111342+GFoster-ISY@users.noreply.github.com>
2020-01-17 09:54:55 +0630

    Calendar Popup is now persistent
    The calendar popup is now persistent
    Code has been tidyied up
    Some styling has been added

--- content.js
+++ content.js
@@ -1,113 +1,98 @@
 ( function () {
 	"use strict";
-	var gcUI = {};
 	
 	var dateSelector = "[aria-label='Due date & time']";
 	var activeSelector = null;
 	var datePopupSelector = "table[role=presentation]";
-	var AssessmentShown = false;
+   var AssessmentShown = false;
+   var calendarDialog = createCalendarDialog();
 
  
-  function pollDOM (){
-    console.log("ISY Classroom - looking for " + activeSelector);
-	const el = document.querySelector(activeSelector);
-	if (el != null) {
-		console.log("ISY Classroom - " + activeSelector + " found.");
-		if (!AssessmentShown){
-			extendDateSelector(el);
-			AssessmentShown = true;
-		}
-	} else {
-		AssessmentShown = false;
-	}
-	setTimeout(pollDOM, 300);
-  }
+   function pollDOM (){
+      const el = document.querySelector(activeSelector);
+      if (el != null) {
+         if (!AssessmentShown){
+            extendDateSelector(el);
+            AssessmentShown = true;
+         }
+      } else {
+         AssessmentShown = false;
+      }
+      setTimeout(pollDOM, 300);
+   }
 
-  function extendDateSelector(element){
-	console.log(element);
-	var title = element.childNodes[0];
-	title.classList.add("isy-injection");
-	var calendarTable = document.querySelector(datePopupSelector);
-	if (!calendarTable)
-		return;
-	var calendarPopup = calendarTable.parentNode;
-	calendarPopup.innerHTML = "";
-	
-	var dialog = createDialog(
-      "ISY SUMMATIVE ASSIGNMENT",
-      "isy-logo",
-      null,
-      null
-    );
-    var iframe = document.createElement("iframe");
-	var myContent = '<!DOCTYPE html>'
-    + '<html><head><title>SUMMATIVE ASSIGNMENT</title></head>'
-    + '<body><p>We need some stuff here</p></body></html>';
-
-	iframe.classList = "isy-editor";
-	iframe.src="javascript:'"+myContent+"'";
-    dialog.body.appendChild(iframe);
-    calendarPopup.appendChild(dialog.node);
-	
-  }
+   function extendDateSelector(element){
+      var title = element.childNodes[0];
+      title.classList.add("isy-injection");
+      var calendarTable = document.querySelector(datePopupSelector);
+      if (!calendarTable)
+         return;
+      var calendarPopup = calendarTable.parentNode;
+      calendarPopup.innerHTML = "";
+      calendarPopup.appendChild(calendarDialog.node);
+   }
   
+   function createCalendarDialog(){
+      var dialog = createDialog(
+         "ISY SUMMATIVE ASSIGNMENT",
+         "isy-medium-logo",
+         closeCalendarAction
+      );
+      var iframe = document.createElement("iframe");
+      var myContent = '<!DOCTYPE html>'
+      + '<html><head><title>SUMMATIVE ASSIGNMENT</title></head>'
+      + '<body><p>We need some stuff here</p></body></html>';
 
+      iframe.classList = "isy-editor";
+      iframe.src="javascript:'"+myContent+"'";
+      dialog.body.appendChild(iframe);
+      return dialog;
+   }
 
-  function waitForLoad() {
-	console.log("ISY Classroom - wait for chrome extension to load");
-	activeSelector = dateSelector;
-	pollDOM();
-  }
+   function closeCalendarAction(div){
+      div.parentNode.style.display = 'none';
+   }
 
-  function createDialog(title, iconClass, backgroundColor, textColor) {
-    backgroundColor =
-      backgroundColor ||
-      window
-        .getComputedStyle(document.querySelector("nav"))
-        .getPropertyValue("background-color");
-    textColor =
-      textColor ||
-      window
-        .getComputedStyle(document.querySelector("nav h1"))
-        .getPropertyValue("color");
+   function waitForLoad() {
+      activeSelector = dateSelector;
+      pollDOM();
+   }
 
-    var div = document.createElement("div");
-    div.classList = "isy-dialog";
+   function createDialog(title, iconClass, closeAction) {
+      var div = document.createElement("div");
+      div.classList = "isy-dialog";
 
-    var header = document.createElement("div");
-    header.classList = "isy-dialog-header";
-    header.style.backgroundColor = backgroundColor;
-    div.appendChild(header);
+      var header = document.createElement("div");
+      header.classList = "isy-dialog-header";
+      div.appendChild(header);
 
-    var icon = document.createElement("i");
-    icon.classList = iconClass;
-    header.appendChild(icon);
+      var icon = document.createElement("i");
+      icon.classList = iconClass;
+      header.appendChild(icon);
 
-    var titleEl = document.createElement("span");
-    titleEl.classList = "isy-dialog-header-title";
-    titleEl.innerText = title;
-    titleEl.style.color = textColor;
+      var titleEl = document.createElement("span");
+      titleEl.classList = "isy-dialog-header-title";
+      titleEl.innerText = title;
 
-    header.appendChild(titleEl);
+      header.appendChild(titleEl);
 
-    var closebtn = document.createElement("span");
-    closebtn.classList = "isy-dialog-close-button";
-    closebtn.style.color = textColor;
-    closebtn.innerHTML = "&times;";
-    closebtn.onclick = function() {
-      div.parentNode.removeChild(div);
-    };
-    header.appendChild(closebtn);
+      var closebtn = document.createElement("span");
+      closebtn.classList = "isy-dialog-close-button";
+      closebtn.innerHTML = "&times;";
+      closebtn.onclick = function() {
+         closeAction(div);
+      };
+      header.appendChild(closebtn);
 
-    var body = document.createElement("div");
-    body.classList = "isy-dialog-body";
-    div.appendChild(body);
+      var body = document.createElement("div");
+      body.classList = "isy-dialog-body";
+      div.appendChild(body);
 
-    return {
-      body: body,
-      node: div
-    };
-  }
+      return {
+         body: body,
+         node: div
+      };
+   }
 
-  waitForLoad();
+   waitForLoad();
 })();
