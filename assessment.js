eee3c7d01ea8d3db86bbdb16dcaada80948c984f
GFoster-ISY <48111342+GFoster-ISY@users.noreply.github.com>
2020-02-05 08:43:40 +0630

    Store the sheet data in Student objects
    Tidy up the code that makes the call with the Google Sheet, so that it creates Student Objects and stores each of these in the studentList of the Assessment object.
    
    NOTE: The code is still in the wrong place, this will need to be called after the classes and number of students has been identified.

--- assessment.js
+++ assessment.js
@@ -19,36 +19,31 @@ class Assessment{
     getStudentList(){
         return this._studentList;
     }
+
+    storeStudentList(xhttp){
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText);
+            var list = JSON.parse(xhttp.responseText);
+            console.log(list);
+            for (var id in list){
+                console.log(id + " : " + list[id]);
+                this._studentList[id] = new Student(id, list[id]);
+            }
+        }
+    }
     setStudentList(ids){
         // get the studentlist from the Google Sheet
         var URL = "https://script.google.com/a/isyedu.org/macros/s/AKfycbw1cM8fnqav_mnJVLOC6F_h2U1dme7KhtE5l-rmioM/dev";
         var DATA = "[32,55,27,18]";
         URL = URL + "?ids=" + DATA;
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+        var assessmentObj = this;
+        xhttp.onreadystatechange=function () {
             console.log("Response received: "+this);
-            if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText)
-//                this._studentList[id] = this.responseText;
-//              document.getElementById("demo").innerHTML = this.responseText;
-            }
-          };
+            assessmentObj.storeStudentList(this);
+        };
         xhttp.open("GET", URL, true);
         xhttp.send();
-        // var ss = SpreadsheetApp.openById("1pxJYSemUuAoMPrzkMBw4sy4o4pCVFRj6-nuUAlnqgcg");
-        // var sheet = ss.getSheetByName("Student");
-        // var range = sheet.getRange(1, 1, sheet.getLastRow());
-        // var values = range.getValues();
-
-        // for (id in ids){
-        //     for (s in values){
-        //         if (s[0] == id){
-        //             name = s[1];
-        //             break;
-        //         }
-        //     }
-        //     this._studentList[id] = new Student(id, name);
-        // }
     }
 
 }
