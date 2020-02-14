eb5a5c7ce7c4d35125810ef10798f82788f432cc
GFoster-ISY <48111342+GFoster-ISY@users.noreply.github.com>
2020-02-14 21:53:32 +0630

    Get course data from the classroom

--- assessment.js
+++ assessment.js
@@ -1,9 +1,14 @@
 
 class Assessment{
+
     constructor(month, studentIDs){
+        this.URL = "https://script.google.com/a/isyedu.org/macros/s/AKfycbw1cM8fnqav_mnJVLOC6F_h2U1dme7KhtE5l-rmioM/dev?action=";
         this._id_month = month;
+        this._courseList = {};
         this._studentList = {};
+        this.getCourseList();
         this.setStudentList(studentIDs);
+        this._activeCourses = {};
     }
 
     get month(){
@@ -20,6 +25,43 @@ class Assessment{
         return this._studentList;
     }
 
+    storeCourseList(xhttp){
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            // Scrape the active class name from the HTML
+            const courseSelector = "a[target=\"_blank\"][data-focus-id] span";
+            const el = document.querySelector(courseSelector);
+            const className = el.innerHTML;
+            console.log("Active class : "+className);
+
+            console.log(xhttp.responseText);
+            var list = JSON.parse(xhttp.responseText);
+            console.log(list);
+            for (var id in list){
+                console.log(id + " : " + list[id]);
+                var course =  new Course(id, list[id]);
+                this._courseList[id] = course;
+                if (list[id] == className){
+                    this._activeCourses [id] = course;
+                    console.log("Active Course " + id + " : " + list[id]);
+                }
+            }
+        }
+    }
+
+    getCourseList(){
+        // Get a list of all courses
+        var url = this.URL + "course";
+        var xhttp = new XMLHttpRequest();
+        var assessmentObj = this;
+        xhttp.onreadystatechange=function () {
+            console.log("Response received: "+this);
+            assessmentObj.storeCourseList(this);
+        };
+        console.log("URL: "+url);
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    }
+
     storeStudentList(xhttp){
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             console.log(xhttp.responseText);
@@ -32,24 +74,21 @@ class Assessment{
         }
     }
     setStudentList(ids){
+        // set up the necessary data 
+        var url  = this.URL + "student";
         // get the student ids from the classroom
 
-        const courseSelector = "a[target=\"_blank\"][data-focus-id] span";
-        const el = document.querySelector(courseSelector);
-        const className = el.innerHTML;
-        console.log("Active class : "+className);
 
         // get the student details from the Google Sheet
-        var URL = "https://script.google.com/a/isyedu.org/macros/s/AKfycbw1cM8fnqav_mnJVLOC6F_h2U1dme7KhtE5l-rmioM/dev";
         var DATA = "[32,55,27,18]";
-        URL = URL + "?ids=" + DATA;
+        url = URL + "&&ids=" + DATA;
         var xhttp = new XMLHttpRequest();
         var assessmentObj = this;
         xhttp.onreadystatechange=function () {
             console.log("Response received: "+this);
             assessmentObj.storeStudentList(this);
         };
-        xhttp.open("GET", URL, true);
+        xhttp.open("GET", url, true);
         xhttp.send();
     }
 
